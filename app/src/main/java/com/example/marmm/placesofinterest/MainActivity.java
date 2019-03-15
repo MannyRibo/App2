7ce1b7515dfb335c24e151fe50d9fdc1cab3b09b
Gino Marciano <gino.bolt@hva.nl>
2019-03-15 17:25:23 +0100

    Push

---
+++ app/src/main/java/com/example/marmm/placesofinterest/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.marmm.placesofinterest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecyclerView.OnItemTouchListener {
+
+    List<GeoObject> mGeoObjects;
+    private GestureDetector mGestureDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGeoObjects = new ArrayList<>();
+
+        for (int i = 0; i < GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES.length; i++) {
+
+            mGeoObjects.add(new GeoObject(GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES[i],
+                    GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i], GeoObject.PRE_DEFINED_GEO_OBJECT_BOOLEANS[i]));
+        }
+
+        final RecyclerView mGeoRecyclerView = findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+
+        mGeoRecyclerView.setLayoutManager(mLayoutManager);
+        mGeoRecyclerView.setHasFixedSize(true);
+        GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+        mGeoRecyclerView.setAdapter(mAdapter);
+        mGeoRecyclerView.addOnItemTouchListener(this);
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+                new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+                    @Override
+                    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                        int position = (viewHolder.getAdapterPosition());
+
+                        GeoObject obj = mGeoObjects.get(position);
+
+                        if ((obj.getisInEuropa() && swipeDir == ItemTouchHelper.LEFT) ||
+                                (!obj.getisInEuropa() && swipeDir == ItemTouchHelper.RIGHT)) {
+                            Toast.makeText(MainActivity.this, "Het antwoord is goed", Toast.LENGTH_LONG).show();
+                        } else  {
+                            Toast.makeText(MainActivity.this, "Het antwoord is niet goed", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mGeoRecyclerView);
+
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View child = rv.findChildViewUnder(e.getX(), e.getY());
+        int mAdapterPosition = rv.getChildAdapterPosition(child);
+        if (child != null && mGestureDetector.onTouchEvent(e)) {
+            Toast.makeText(this, mGeoObjects.get(mAdapterPosition).getmGeoName(), Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+    }
+}
